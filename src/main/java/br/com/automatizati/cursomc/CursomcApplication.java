a7303bfe7291a2bae3397cf1c45810fe81a08399
Flabian Martinez <flabian.martinez@gmail.com>
2017-10-14 13:43:28 -0300

    pedido, pagamento e estadoPagamento

--- src/main/java/br/com/automatizati/cursomc/CursomcApplication.java
+++ src/main/java/br/com/automatizati/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package br.com.automatizati.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,35 +13,48 @@ import br.com.automatizati.cursomc.domain.Cidade;
 import br.com.automatizati.cursomc.domain.Cliente;
 import br.com.automatizati.cursomc.domain.Endereco;
 import br.com.automatizati.cursomc.domain.Estado;
+import br.com.automatizati.cursomc.domain.Pagamento;
+import br.com.automatizati.cursomc.domain.PagamentoComBoleto;
+import br.com.automatizati.cursomc.domain.PagamentoComCartao;
+import br.com.automatizati.cursomc.domain.Pedido;
 import br.com.automatizati.cursomc.domain.Produto;
+import br.com.automatizati.cursomc.domain.enums.EstadoPagamento;
 import br.com.automatizati.cursomc.domain.enums.TipoCliente;
 import br.com.automatizati.cursomc.repositories.CategoriaRepository;
 import br.com.automatizati.cursomc.repositories.CidadeRepository;
 import br.com.automatizati.cursomc.repositories.ClienteRepository;
 import br.com.automatizati.cursomc.repositories.EnderecoRepository;
 import br.com.automatizati.cursomc.repositories.EstadoRepository;
+import br.com.automatizati.cursomc.repositories.PagamentoRepository;
+import br.com.automatizati.cursomc.repositories.PedidoRepository;
 import br.com.automatizati.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
+public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -50,46 +64,60 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... arg0) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2 , p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
+
 		estadoRepository.save(Arrays.asList(est1, est2));
-		cidadeRepository.save(Arrays.asList(c1, c2, c3 ));
-		
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "28725673899", TipoCliente.PESSOAFISICA);
-		
+
 		cli1.getTelefones().addAll(Arrays.asList("3256542", "998564525"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303","Jardim", "3213948", cli1, c1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "3105", "Sala 800","Centro", "erer948", cli1, c2);
-		
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "3213948", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "3105", "Sala 800", "Centro", "erer948", cli1, c2);
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
+
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy hh:mm");
 		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
 		
+		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pgto1);
+		
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pgto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1, ped2));
+		pagamentoRepository.save(Arrays.asList(pgto1, pgto2));
 	}
 }
---
+++ src/main/java/br/com/automatizati/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,31 @@
+package br.com.automatizati.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import br.com.automatizati.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
+	
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+
+}
