e6b0e1ce830e0c173e8c64535402cd885c02d90c
DESKTOP-2JO0COT\Flabian <flabian.martinez@gmail.com>
2017-11-10 19:31:01 -0300

    Criando profile de teste

---
+++ src/main/java/br/com/automatizati/cursomc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package br.com.automatizati.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.automatizati.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
