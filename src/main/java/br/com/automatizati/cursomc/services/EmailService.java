9c0217871479db2a4a42c55bca2207df95b1a2bf
DESKTOP-2JO0COT\Flabian <flabian.martinez@gmail.com>
2017-11-11 11:11:18 -0300

    MockEmailService com Logger. Padrões Strategy e Template

---
+++ src/main/java/br/com/automatizati/cursomc/services/EmailService.java
@@ -0,0 +1,12 @@
+package br.com.automatizati.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import br.com.automatizati.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+}
