b26ccb1dbd9fd90c5003988301a2ed1229c28929
DESKTOP-2JO0COT\Flabian <flabian.martinez@gmail.com>
2017-11-11 20:44:50 -0300

    Adicionando senha a Cliente

--- src/main/java/br/com/automatizati/cursomc/services/DBService.java
+++ src/main/java/br/com/automatizati/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.automatizati.cursomc.domain.Categoria;
@@ -32,6 +33,9 @@ import br.com.automatizati.cursomc.repositories.ProdutoRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -117,7 +121,7 @@ public class DBService {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nikolassoares84@gmail.com", "28725673899", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "nikolassoares84@gmail.com", "28725673899", TipoCliente.PESSOAFISICA, pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("3256542", "998564525"));
 
